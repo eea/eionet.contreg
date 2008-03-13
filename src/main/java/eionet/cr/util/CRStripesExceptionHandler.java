31f4a1d38a1a40d70ca5cec670b336f8b8567938
Jaanus Heinlaid <jaanus.heinlaid@gmail.com>
2008-03-13 13:27:51 +0000

    Find root cause recursively. As a result, we get a more meaningful exception thrown to the user.

--- src/main/java/eionet/cr/util/CRStripesExceptionHandler.java
+++ src/main/java/eionet/cr/util/CRStripesExceptionHandler.java
@@ -32,17 +32,31 @@ public class CRStripesExceptionHandler implements ExceptionHandler {
      * (non-Javadoc)
      * @see net.sourceforge.stripes.exception.ExceptionHandler#handle(java.lang.Throwable, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
      */
-    public void handle(Throwable throwable,
+    public void handle(Throwable t,
                        HttpServletRequest request,
                        HttpServletResponse response) throws ServletException, IOException {
     	
-    	Throwable t = (throwable instanceof ServletException) ? ((ServletException)throwable).getRootCause() : throwable;
-    	if (t==null)
-    		t = throwable;
+    	Throwable newThrowable = (t instanceof ServletException) ? getRootCause((ServletException)t) : t;
+    	if (newThrowable==null)
+    		newThrowable = t;
 
-    	logger.error(t.getMessage(), t);
-    	request.setAttribute("exception", t);
+    	logger.error(newThrowable.getMessage(), newThrowable);
+    	request.setAttribute("exception", newThrowable);
     	request.getRequestDispatcher("/pages/error.jsp").forward(request, response);
     	
+    }
+
+    /**
+     * 
+     * @param servletException
+     * @return
+     */
+    private Throwable getRootCause(ServletException servletException){
+    	
+    	Throwable rootCause = servletException.getRootCause();
+    	if (rootCause instanceof ServletException)
+    		return getRootCause((ServletException)rootCause);
+    	else
+    		return rootCause;
     }
 }
