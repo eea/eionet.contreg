1fd0d3d2224c2a5607595de9040d4fd8af87376f
Jaanus Heinlaid <jaanus.heinlaid@gmail.com>
2008-09-26 09:20:09 +0000

    Some changes for unti-testing environment

--- src/main/java/eionet/cr/config/GeneralConfig.java
+++ src/main/java/eionet/cr/config/GeneralConfig.java
@@ -55,6 +55,7 @@ public class GeneralConfig {
 	private static void init(){
 		properties = new Properties();
 		try{
+			String s = GeneralConfig.class.getClassLoader().getResource(PROPERTIES_FILE_NAME).toString();
 			properties.load(GeneralConfig.class.getClassLoader().getResourceAsStream(PROPERTIES_FILE_NAME));
 		}
 		catch (IOException e){
