4ca04309aadd5fe8516cef01db83149ee5a4be16
Jaanus Heinlaid <jaanus.heinlaid@gmail.com>
2009-03-26 15:05:41 +0000

    Refactoring

--- src/main/java/eionet/cr/search/DiscoveredSinceTimeSearch.java
+++ src/main/java/eionet/cr/search/DiscoveredSinceTimeSearch.java
@@ -5,18 +5,21 @@ import java.util.List;
 
 import eionet.cr.common.Predicates;
 import eionet.cr.util.Hashes;
+import eionet.cr.util.pagination.Pagination;
 
 public class DiscoveredSinceTimeSearch extends AbstractSubjectSearch {
 	
 	/** */
 	private Date time;
+	private int maxResults;
 	
 	/**
 	 * 
 	 * @param time
 	 */
-	public DiscoveredSinceTimeSearch(Date time){
+	public DiscoveredSinceTimeSearch(Date time, int maxResults){
 		this.time = time;
+		this.maxResults= maxResults;
 	}
 
 	/*
@@ -32,20 +35,10 @@ public class DiscoveredSinceTimeSearch extends AbstractSubjectSearch {
 		append("select sql_calc_found_rows distinct SPO.SUBJECT as SUBJECT_HASH from SPO, RESOURCE").
 		append(" where SPO.SUBJECT=RESOURCE.URI_HASH ").
 		append(" and RESOURCE.FIRSTSEEN_TIME>?").
-		append(" order by RESOURCE.FIRSTSEEN_TIME desc");
+		append(" order by RESOURCE.FIRSTSEEN_TIME desc").
+		append(" limit ").append(maxResults);
 		
 		inParameters.add(Long.valueOf(time.getTime()));
-		
-		if (!noLimit){
-			if (pageLength>0){
-				sqlBuf.append(" limit ");
-				if (pageNumber>0){
-					sqlBuf.append("?,");
-					inParameters.add(new Integer((pageNumber-1)*pageLength));
-				}
-				sqlBuf.append(pageLength);
-			}
-		}
 		
 		return sqlBuf.toString();
 	}
