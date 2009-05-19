d7522ab594e1b6d746c701f35fabdb3042443f8f
Søren Roug <soren.roug@eea.europa.eu>
2009-05-19 13:44:03 +0000

    More tests on various configurations

--- src/test/java/eionet/cr/test/JavaURLTest.java
+++ src/test/java/eionet/cr/test/JavaURLTest.java
@@ -3,6 +3,10 @@ import java.net.URL;
 import junit.framework.TestCase;
 
 public class JavaURLTest extends TestCase {
+	/**
+	 * This method tests the rdf:about="..." when the base URL ends with a "/"
+	 * @throws Exception
+	 */
 	public void testURLWithSlash() throws Exception {
 		URL basename = new URL("http://rs.tdwg.org/dwc/terms/");
 		assertEquals(basename.toString(),"http://rs.tdwg.org/dwc/terms/");
@@ -12,9 +16,19 @@ public class JavaURLTest extends TestCase {
 	
 		taxon  = new URL(basename, "sub/Taxon");
 		assertEquals(taxon.toString(),"http://rs.tdwg.org/dwc/terms/sub/Taxon");
+
+		URL samesame = new URL(basename, "");
+		assertEquals(samesame.toString(),"http://rs.tdwg.org/dwc/terms/");
 		
+		// Test rdf:ID="kingdom" == rdf:about="#kingdom"
+		URL rdfid = new URL(basename, "#kingdom");
+		assertEquals(rdfid.toString(),"http://rs.tdwg.org/dwc/terms/#kingdom");
 	}
 
+	/**
+	 * This method tests the rdf:about="..." when the base url ends with a real file.
+	 * @throws Exception
+	 */
 	public void testURLWithHash() throws Exception {
 		URL basename = new URL("http://rs.tdwg.org/dwc/terms/something.rdf#");
 		assertEquals(basename.toString(),"http://rs.tdwg.org/dwc/terms/something.rdf#");
@@ -22,9 +36,18 @@ public class JavaURLTest extends TestCase {
 		URL taxon = new URL(basename, "Taxon");
 		assertEquals(taxon.toString(),"http://rs.tdwg.org/dwc/terms/Taxon");
 	
+		// Test rdf:about="sub/Taxon"
 		taxon  = new URL(basename, "sub/Taxon");
 		assertEquals(taxon.toString(),"http://rs.tdwg.org/dwc/terms/sub/Taxon");
 		
+		// Test rdf:about=""
+		URL samesame = new URL(basename, "");
+		assertEquals(samesame.toString(),"http://rs.tdwg.org/dwc/terms/something.rdf#");
+
+		// Test rdf:ID="kingdom" == rdf:about="#kingdom"
+		URL rdfid = new URL(basename, "#kingdom");
+		assertEquals(rdfid.toString(),"http://rs.tdwg.org/dwc/terms/something.rdf#kingdom");
+
 	}
 
 }
