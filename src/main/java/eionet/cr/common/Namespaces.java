f31fc27f714d7ad2b70138aaff865dda158283b6
Jaanus Heinlaid <jaanus.heinlaid@gmail.com>
2010-01-11 14:14:03 +0000

    Improvements to the AMP RDF feed.

---
+++ src/main/java/eionet/cr/common/Namespaces.java
@@ -0,0 +1,38 @@
+/*
+* The contents of this file are subject to the Mozilla Public
+* 
+* License Version 1.1 (the "License"); you may not use this file
+* except in compliance with the License. You may obtain a copy of
+* the License at http://www.mozilla.org/MPL/
+* 
+* Software distributed under the License is distributed on an "AS
+* IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+* implied. See the License for the specific language governing
+* rights and limitations under the License.
+* 
+* The Original Code is Content Registry 2.0.
+* 
+* The Initial Owner of the Original Code is European Environment
+* Agency. Portions created by Tieto Eesti are Copyright
+* (C) European Environment Agency. All Rights Reserved.
+* 
+* Contributor(s):
+* Jaanus Heinlaid, Tieto Eesti*/
+package eionet.cr.common;
+
+/**
+ * 
+ * @author <a href="mailto:jaanus.heinlaid@tietoenator.com">Jaanus Heinlaid</a>
+ *
+ */
+public interface Namespaces {
+
+	/** */
+	public static final String RDF = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
+	public static final String RDFS = "http://www.w3.org/2000/01/rdf-schema#";
+	public static final String OWL = "http://www.w3.org/2002/07/owl#";
+	public static final String DC = "http://purl.org/dc/elements/1.1/";
+	public static final String CR = "http://cr.eionet.europa.eu/ontologies/contreg.rdf#";
+	public static final String IMS = "http://rdfdata.eionet.europa.eu/ims/ontology/";
+	public static final String AMP = "http://rdfdata.eionet.europa.eu/amp/ontology/";
+}
