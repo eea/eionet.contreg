2ab068db33e6c9c24efcd8c05c3b8f6933cd4bbf
Risto Alt <risto@tripledev.ee>
2011-03-27 13:28:49 +0000

    Better checkstyle

--- src/main/java/eionet/cr/web/action/SPARQLEndpointActionBean.java
+++ src/main/java/eionet/cr/web/action/SPARQLEndpointActionBean.java
@@ -16,7 +16,6 @@ import org.openrdf.OpenRDFException;
 import org.openrdf.query.QueryLanguage;
 import org.openrdf.query.TupleQuery;
 import org.openrdf.query.TupleQueryResult;
-import org.openrdf.query.resultio.sparqljson.SPARQLResultsJSONWriter;
 import org.openrdf.query.resultio.sparqlxml.SPARQLResultsXMLWriter;
 import org.openrdf.repository.Repository;
 import org.openrdf.repository.RepositoryConnection;
@@ -34,7 +33,7 @@ import eionet.cr.web.sparqlClient.helpers.QueryResult;
  *
  */
 @UrlBinding("/sparql")
-public class SPARQLEndpointActionBean extends AbstractActionBean{
+public class SPARQLEndpointActionBean extends AbstractActionBean {
     
     private static final String FORMAT_XML = "xml";
     private static final String FORMAT_JSON = "json";
@@ -59,25 +58,28 @@ public class SPARQLEndpointActionBean extends AbstractActionBean{
      * @throws OpenRDFException
      */
     @DefaultHandler
-    public Resolution execute() throws OpenRDFException{
+    public Resolution execute() throws OpenRDFException {
         
         String acceptHeader = getContext().getRequest().getHeader("accept");
         String[] accept = null;
-        if(acceptHeader != null && acceptHeader.length() > 0)
+        if (acceptHeader != null && acceptHeader.length() > 0) {
             accept = acceptHeader.split(",");
+        }
         
-        if(!StringUtils.isBlank(format))
+        if (!StringUtils.isBlank(format)) {
             accept[0] = format;
+        }
         
-        if(nrOfHits == 0)
+        if (nrOfHits == 0) {
             nrOfHits = 20;
+        }
 
-        if(accept != null && accept[0].equals("text/html")){
-            if (!StringUtils.isBlank(query)){
+        if (accept != null && accept[0].equals("text/html")) {
+            if (!StringUtils.isBlank(query)) {
                 runQuery(query, FORMAT_HTML, null);
             }
             return new ForwardResolution(FORM_PAGE);
-        } else if(accept != null && accept[0].equals("application/sparql-results+json")){
+        } else if (accept != null && accept[0].equals("application/sparql-results+json")) {
             return new StreamingResolution("application/sparql-results+json") {
                 public void stream(HttpServletResponse response) throws Exception {
                     runQuery(query, FORMAT_JSON, response.getOutputStream());
@@ -94,7 +96,7 @@ public class SPARQLEndpointActionBean extends AbstractActionBean{
     
     private void runQuery(String query, String format, OutputStream out) {
         
-        if (!StringUtils.isBlank(query)){
+        if (!StringUtils.isBlank(query)) {
             String url = GeneralConfig.getProperty("virtuoso.db.url");
             String username = GeneralConfig.getProperty("virtuoso.db.usr");
             String password = GeneralConfig.getProperty("virtuoso.db.pwd");
@@ -107,13 +109,13 @@ public class SPARQLEndpointActionBean extends AbstractActionBean{
                 try{
                     TupleQuery resultsTable = con.prepareTupleQuery(QueryLanguage.SPARQL, query);
                     
-                    if(format != null && format.equals(FORMAT_XML)){
+                    if (format != null && format.equals(FORMAT_XML)) {
                         SPARQLResultsXMLWriter sparqlWriter = new SPARQLResultsXMLWriter(out);
                         resultsTable.evaluate(sparqlWriter);
-                    } else if(format != null && format.equals(FORMAT_JSON)){
+                    } else if (format != null && format.equals(FORMAT_JSON)) {
                         CRJsonWriter sparqlWriter = new CRJsonWriter(out);
                         resultsTable.evaluate(sparqlWriter);
-                    } else if(format != null && format.equals(FORMAT_HTML)){
+                    } else if (format != null && format.equals(FORMAT_HTML)) {
                         long startTime = System.currentTimeMillis();
                         TupleQueryResult bindings = resultsTable.evaluate();
                         executionTime = System.currentTimeMillis() - startTime;
