7cf288cb6482d5d719a78fef4088d83c445e35b2
Kaido Laine <kaido@tripledev.ee>
2011-05-26 11:04:00 +0000

    junit test for FreetextSearch

---
+++ src/test/java/eionet/cr/dao/MockVirtuosoBaseDAOTest.java
@@ -0,0 +1,153 @@
+package eionet.cr.dao;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openrdf.model.Statement;
+import org.openrdf.model.Value;
+import org.openrdf.query.BindingSet;
+import org.openrdf.query.algebra.evaluation.QueryBindingSet;
+import org.openrdf.rio.RDFHandler;
+import org.openrdf.rio.RDFHandlerException;
+import org.openrdf.rio.RDFParseException;
+import org.openrdf.rio.RDFParser;
+import org.openrdf.rio.n3.N3ParserFactory;
+
+import eionet.cr.dao.readers.ResultSetReaderException;
+import eionet.cr.dao.virtuoso.VirtuosoBaseDAO;
+import eionet.cr.util.sesame.SPARQLResultSetReader;
+
+/**
+ * Mock for testing queries. Working only for testing readers. Each
+ * implementing class must have a corresponding file in N3 format that can be
+ * exported from SPARQL endpoint.
+ */
+public class MockVirtuosoBaseDAOTest extends VirtuosoBaseDAO {
+    /** Tag name in N3.*/
+    private static final String RESULTVARIABLE_NAME = "http://www.w3.org/2005/sparql-results#resultVariable";
+    /** Tag name in N3.*/
+    private static final String VARIABLE_NAME = "http://www.w3.org/2005/sparql-results#variable";
+    /** Tag name in N3.*/
+    private static final String VALUE_NAME = "http://www.w3.org/2005/sparql-results#value";
+    /** Tag name in N3.*/
+    private static final String SOLUTION_NAME = "http://www.w3.org/2005/sparql-results#solution";
+
+    /**
+     * Fake list of bindingSet.
+     */
+    private ArrayList<QueryBindingSet> bindingSet;
+    /** custom RDF handler. */
+    private MockRDFHandler rdfHandler = new MockRDFHandler();
+    /** binding names araylist.*/
+    private List<String> bindingNames = new ArrayList<String>();
+
+    /**
+     * Default construtor. File name with test data is given as the parameter.
+     * Supports files in N3 format exported from SPARQL endpoint.
+     * @param fileName Test data file name in test-resources.
+     */
+    public MockVirtuosoBaseDAOTest(String fileName) {
+        bindingSet = new ArrayList<QueryBindingSet>();
+        // TODO study why RDF reader is not working properly
+        // RDFParserFactory factory = new
+        // RDFParserRegistry().get(RDFFormat.forFileName(fileName));
+        N3ParserFactory factory = new N3ParserFactory();
+        RDFParser parser = factory.getParser();
+
+        parser.setStopAtFirstError(false);
+        parser.setRDFHandler(rdfHandler);
+
+        try {
+            parser.parse(this.getClass().getClassLoader().getResourceAsStream(fileName), "http://127.0.0.1/test");
+
+        } catch (RDFParseException e) {
+            e.printStackTrace();
+        } catch (RDFHandlerException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected <T> List<T> executeSPARQL(String sparql, SPARQLResultSetReader<T> reader) throws DAOException {
+
+        for (BindingSet binding : bindingSet) {
+            try {
+                reader.readRow(binding);
+            } catch (ResultSetReaderException e) {
+                e.printStackTrace();
+            }
+        }
+        return reader.getResultList();
+    }
+
+
+    /**
+     * internal RDF handler for creating fake bindingset objects.
+     */
+    class MockRDFHandler implements RDFHandler {
+        private boolean bindingsAlreadyAdded = false;
+
+        private QueryBindingSet currentBinding;
+        private String currentVarName;
+
+        @Override
+        public void endRDF() throws RDFHandlerException {
+            // add last value
+            if (currentBinding != null) {
+                bindingSet.add(currentBinding);
+            }
+
+        }
+
+        @Override
+        public void handleComment(String arg0) throws RDFHandlerException {
+
+        }
+
+        @Override
+        public void handleNamespace(String arg0, String arg1) throws RDFHandlerException {
+
+        }
+
+        /**
+         * creates a fake bindingset object.
+         */
+        public void handleStatement(Statement statement) throws RDFHandlerException {
+            // new binding started:
+            if (statement.getPredicate() != null && statement.getPredicate().stringValue().equals(SOLUTION_NAME)) {
+                if (!bindingsAlreadyAdded) {
+                    bindingsAlreadyAdded = true;
+                } else {
+                    bindingSet.add(currentBinding);
+                }
+                currentBinding = new QueryBindingSet();
+            }
+            // its a binding name
+            if (statement.getPredicate() != null && statement.getPredicate().stringValue().equals(RESULTVARIABLE_NAME)) {
+                bindingNames.add(statement.getObject().stringValue());
+            }
+            // new variable Starts
+            if (statement.getPredicate() != null && statement.getPredicate().stringValue().equals(VARIABLE_NAME)) {
+                currentVarName = statement.getObject().stringValue();
+            }
+
+            // actual value
+            if (statement.getPredicate() != null && statement.getPredicate().stringValue().equals(VALUE_NAME)) {
+                Value value = statement.getObject();
+                currentBinding.addBinding(currentVarName, value);
+            }
+        }
+
+        @Override
+        public void startRDF() throws RDFHandlerException {
+        }
+    }
+
+
+}
---
+++ src/test/java/eionet/cr/dao/virtuoso/FreetextSearchTest.java
@@ -0,0 +1,36 @@
+package eionet.cr.dao.virtuoso;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import eionet.cr.dao.DAOException;
+import eionet.cr.dao.MockVirtuosoBaseDAOTest;
+import eionet.cr.dao.readers.FreeTextSearchReader;
+/**
+ * Tests Freetext search result processing.
+ */
+public class FreetextSearchTest extends MockVirtuosoBaseDAOTest {
+    private static final String FILE_NAME = "test-freetext.nt";
+
+    public FreetextSearchTest() {
+        super(FILE_NAME);
+    }
+
+    @Test
+    public void testFreetextQuery() {
+        //dummy SQL just for understanding how the N3 file was generated
+        String sparql = "select distinct ?s where { ?s ?p ?o .   FILTER bif:contains(?o, \"'GEMET'\")}";
+        FreeTextSearchReader<String> reader = new FreeTextSearchReader<String>();
+        List<String> results =  null;
+        try {
+            results =  executeSPARQL(sparql, reader);
+        } catch (DAOException e) {
+            e.printStackTrace();
+        }
+        assertTrue(results != null && results.size() == 6);
+    }
+
+}
