09674ab521a3b50764e23ad73b4651974c453e32
Søren Roug <soren.roug@eea.europa.eu>
2011-10-24 12:00:30 +0000

    Fixed #2781 by reverting changeset:10600

---
+++ src/main/java/eionet/cr/web/util/BaseUrl.java
@@ -0,0 +1,19 @@
+package eionet.cr.web.util;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class BaseUrl {
+
+    static boolean valueSet = false;
+    static String baseUrl = "";
+
+    public static String getBaseUrl(HttpServletRequest request) {
+        if (!valueSet) {
+            valueSet = true;
+            StringBuffer requestUrl = request.getRequestURL();
+            String servletPath = request.getServletPath();
+            baseUrl = requestUrl.substring(0, requestUrl.length() - servletPath.length()) + "/";
+        }
+        return baseUrl;
+    }
+}
