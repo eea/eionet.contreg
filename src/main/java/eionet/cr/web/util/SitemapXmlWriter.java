2936b6028a969a4763e92501808f25023cefa9fe
Juhan Voolaid <juhan@tripledev.ee>
2011-10-24 13:55:20 +0000

    #2471 - semantic sitemap functionality

--- src/main/java/eionet/cr/web/util/SitemapXmlWriter.java
+++ src/main/java/eionet/cr/web/util/SitemapXmlWriter.java
@@ -33,18 +33,23 @@ import eionet.cr.dto.UploadDTO;
 
 /**
  * Sitemap xml writer.
- *
+ * 
  * @author Juhan Voolaid
  */
 public class SitemapXmlWriter {
 
     private static final String ENCODING = "UTF-8";
+    private static final String DEFAULT_NS = "http://www.sitemaps.org/schemas/sitemap/0.9";
+    private static final String DEFAULT_NS_LOCATION = "http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd";
     private static final String SCHEMA_NS_URI = "http://sw.deri.org/2007/07/sitemapextension/scschema.xsd";
     private static final String SCHEMA_NS_PREFIX = "sc";
+    private static final String XSI_NS_URI = "http://sw.deri.org/2007/07/sitemapextension/scschema.xsd";
+    private static final String XSI_NS_PREFIX = "xsi";
     private static final String ROOT_ELEMENT = "urlset";
     private static final String DATASET_ELEMENT = "dataset";
     private static final String DATASET_LABEL_ELEMENT = "datasetLabel";
     private static final String DATA_DUMP_LOCATION_ELEMENT = "dataDumpLocation";
+    private static final String LAST_MODIFIED_ELEMENT = "lastmod";
 
     /**
      * XMLWriter to write XML to.
@@ -57,14 +62,17 @@ public class SitemapXmlWriter {
 
     /**
      * Writes sitemap xml into stream based of the uploads data.
-     *
+     * 
      * @param uploads
      * @throws XMLStreamException
      */
     public void writeSitemapXml(List<UploadDTO> uploads) throws XMLStreamException {
         writer.writeStartDocument(ENCODING, "1.0");
         writer.writeStartElement(ROOT_ELEMENT);
+        writer.writeDefaultNamespace(DEFAULT_NS);
         writer.writeNamespace(SCHEMA_NS_PREFIX, SCHEMA_NS_URI);
+        writer.writeNamespace(XSI_NS_PREFIX, XSI_NS_URI);
+        writer.writeAttribute("xsi:schemaLocation", DEFAULT_NS + " " + DEFAULT_NS_LOCATION);
 
         for (UploadDTO upload : uploads) {
             writer.writeStartElement(SCHEMA_NS_PREFIX, DATASET_ELEMENT, SCHEMA_NS_URI);
@@ -73,6 +81,9 @@ public class SitemapXmlWriter {
             writer.writeEndElement();
             writer.writeStartElement(SCHEMA_NS_PREFIX, DATA_DUMP_LOCATION_ELEMENT, SCHEMA_NS_URI);
             writer.writeCharacters(upload.getSubjectUri());
+            writer.writeEndElement();
+            writer.writeStartElement(SCHEMA_NS_PREFIX, LAST_MODIFIED_ELEMENT, SCHEMA_NS_URI);
+            writer.writeCharacters(upload.getDateModified());
             writer.writeEndElement();
             writer.writeEndElement();
         }
