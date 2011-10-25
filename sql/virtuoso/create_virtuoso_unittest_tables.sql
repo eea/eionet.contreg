6cbbc87a6916ad48d9cb63fa8d4a5b27e3a3cc06
Risto Alt <risto@tripledev.ee>
2011-10-25 11:52:44 +0000

    Documentation table doesn't need column "content" anymore

--- sql/virtuoso/create_virtuoso_unittest_tables.sql
+++ sql/virtuoso/create_virtuoso_unittest_tables.sql
@@ -2,7 +2,6 @@ create table CRTEST.cr3test.documentation
 (
 	page_id varchar(255) NOT NULL,
 	content_type varchar(100) NOT NULL default 'text/html',
-	content long varchar NOT NULL,
 	title varchar(512) default '',
 	PRIMARY KEY (page_id)
 );
