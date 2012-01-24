7c888c7545083bfad7245e253c04bdd73930b0bc
Jaanus Heinlaid <jaanus.heinlaid@gmail.com>
2012-01-24 13:03:19 +0000

    Rename the script that enforces CR inference rules + make it download contreg.rdf from SVN repository

--- sql/virtuoso/install/4_enforce_inferene_rules.sql
+++ sql/virtuoso/install/4_enforce_inferene_rules.sql
@@ -3,6 +3,6 @@
 -- Enforce backward inference based on http://cr.eionet.europa.eu/ontologies/contreg.rdf
 -- -------------------------------------------------------------------------------------
 
-DB.DBA.RDF_LOAD_RDFXML (file_to_string('../../../src/main/webapp/ontologies/contreg.rdf'), 'http://cr.eionet.europa.eu/ontologies/contreg.rdf', 'http://cr.eionet.europa.eu/ontologies/contreg.rdf');
+DB.DBA.RDF_LOAD_RDFXML (http_get('http://svn.eionet.europa.eu/projects/Reportnet/browser/cr3/trunk/src/main/webapp/ontologies/contreg.rdf?format=txt'), 'http://cr.eionet.europa.eu/ontologies/contreg.rdf', 'http://cr.eionet.europa.eu/ontologies/contreg.rdf');
 rdfs_rule_set ('CRInferenceRule', 'http://cr.eionet.europa.eu/ontologies/contreg.rdf');
 
