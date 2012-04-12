451d4b847dff083f57a931f60e8a4d33c9d5a494
Enriko Käsper <enriko@tripledev.ee>
2012-04-12 14:26:07 +0000

    Ticket #3017: unit-tests for Virtuoso JDBC driver.

---
+++ src/test/java/eionet/cr/util/virtuoso/VirtuosoConnectionTest.java
@@ -0,0 +1,85 @@
+/*
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * The Original Code is Content Registry 3
+ *
+ * The Initial Owner of the Original Code is European Environment
+ * Agency. Portions created by TripleDev or Zero Technologies are Copyright
+ * (C) European Environment Agency.  All Rights Reserved.
+ *
+ * Contributor(s):
+ *        Enriko Käsper
+ */
+
+package eionet.cr.util.virtuoso;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+import eionet.cr.util.sesame.SesameUtil;
+import eionet.cr.util.sql.SQLUtil;
+
+/**
+ * Test Virtuoso Connection.
+ * 
+ * @author Enriko Käsper
+ */
+public class VirtuosoConnectionTest extends TestCase {
+    /*
+     * (non-Javadoc)
+     * @see junit.framework.TestCase#tearDown()
+     */
+    @Override
+    protected void tearDown() throws Exception {
+        Connection conn = null;
+        try {
+            conn = SesameUtil.getSQLConnection();
+            SQLUtil.executeUpdate("DELETE FROM URGENT_HARVEST_QUEUE WHERE \"URL\"='http://www.google.com'", conn);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            SQLUtil.close(conn);
+        }
+        super.tearDown();
+    }
+
+    /**
+     * Test creating many open statements through Sesame API.
+     */
+    @Test
+    public void testTooManyOpenStmtsSesame() throws IOException, SQLException, InterruptedException {
+        String sql = "insert into URGENT_HARVEST_QUEUE (URL,\"TIMESTAMP\") VALUES (?,NOW())";
+        PreparedStatement ps = null;
+        Connection conn = null;
+        try {
+            conn = SesameUtil.getSQLConnection();
+            ps = conn.prepareStatement(sql);
+            int MAXIT = 10000;
+            for (int i = 0; i < MAXIT; i++) {
+                ps.setString(1, "http://www.google.com");
+                ps.execute();
+                conn.commit();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("can't add data to virtuoso. ");
+        } finally {
+            SQLUtil.close(ps);
+            SQLUtil.close(conn);
+        }
+    }
+}
---
+++ src/test/java/eionet/cr/util/virtuoso/VirtuosoJdbcDriverTest.java
@@ -0,0 +1,84 @@
+/*
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * The Original Code is Content Registry 3
+ *
+ * The Initial Owner of the Original Code is European Environment
+ * Agency. Portions created by TripleDev or Zero Technologies are Copyright
+ * (C) European Environment Agency.  All Rights Reserved.
+ *
+ * Contributor(s):
+ *        Enriko Käsper
+ */
+
+package eionet.cr.util.virtuoso;
+
+import java.io.IOException;
+import java.net.URI;
+import java.sql.CallableStatement;
+import java.sql.SQLException;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+import virtuoso.jdbc3.VirtuosoConnectionPoolDataSource;
+import virtuoso.jdbc3.VirtuosoPooledConnection;
+import eionet.cr.config.GeneralConfig;
+import eionet.cr.test.helpers.RdfLoader;
+
+/**
+ * Test Virtuoso Jdbc driver.
+ * 
+ * @author Enriko Käsper
+ */
+public class VirtuosoJdbcDriverTest extends TestCase {
+
+    /**
+     * Test if CR uses correct Virtuoso jdbc driver. It shouldn't get "Too many open statements" error.
+     */
+    @Test
+    public void testTooManyOpenStmts() throws IOException, SQLException, InterruptedException {
+        VirtuosoConnectionPoolDataSource dbsource = new VirtuosoConnectionPoolDataSource();
+
+        String testDbURI = GeneralConfig.getRequiredProperty(GeneralConfig.VIRTUOSO_DB_URL);
+        URI uri = URI.create(testDbURI.substring(5));
+
+        dbsource.setServerName(uri.getHost());
+        dbsource.setPortNumber(uri.getPort());
+        dbsource.setPassword(GeneralConfig.getRequiredProperty(GeneralConfig.VIRTUOSO_DB_PWD));
+        dbsource.setUser(GeneralConfig.getRequiredProperty(GeneralConfig.VIRTUOSO_DB_USR));
+        dbsource.setCharset("UTF-8");
+        VirtuosoPooledConnection pooledConnection = (VirtuosoPooledConnection) dbsource.getPooledConnection();
+        virtuoso.jdbc3.VirtuosoConnection con = pooledConnection.getVirtuosoConnection();
+        String jdbcComp = "DB.DBA.TTLP (?, ?, ?, ?)";
+        CallableStatement stmt = null;
+        int MAXIT = 10000;
+        for (int i = 0; i < MAXIT; i++) {
+            try {
+                stmt = con.prepareCall(jdbcComp);
+                stmt.setString(1, "");
+                stmt.setString(2, "");
+                stmt.setString(3, RdfLoader.getGraphUri("test.rdf"));
+                stmt.setInt(4, 256);
+                stmt.execute();
+                con.commit();
+            } catch (Exception e) {
+                e.printStackTrace();
+                fail("can't add data to virtuoso. ");
+            } finally {
+                if (stmt != null) {
+                    stmt.close();
+                }
+            }
+        }
+    }
+}
