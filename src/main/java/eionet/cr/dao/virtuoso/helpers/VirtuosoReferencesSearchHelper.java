383c40f10dd2fa3422c3042de703f97b80812ddf
Juhan Voolaid <juhan@tripledev.ee>
2012-11-12 15:39:21 +0000

    SPARQL query fix for big text objects. Refs #3528.

--- src/main/java/eionet/cr/dao/virtuoso/helpers/VirtuosoReferencesSearchHelper.java
+++ src/main/java/eionet/cr/dao/virtuoso/helpers/VirtuosoReferencesSearchHelper.java
@@ -9,6 +9,7 @@ import eionet.cr.util.Bindings;
 import eionet.cr.util.SortingRequest;
 import eionet.cr.util.pagination.PagingRequest;
 import eionet.cr.util.sesame.SPARQLQueryUtil;
+import eionet.cr.web.util.WebConstants;
 import eionet.cr.web.util.columns.ReferringPredicatesColumn;
 
 /**
@@ -30,9 +31,12 @@ public class VirtuosoReferencesSearchHelper extends AbstractSearchHelper {
     /**
      * Creates a new helper object.
      *
-     * @param subjectUri resource subject URI.
-     * @param pagingRequest paging request from the UI
-     * @param sortingRequest sorting request for theresults table
+     * @param subjectUri
+     *            resource subject URI.
+     * @param pagingRequest
+     *            paging request from the UI
+     * @param sortingRequest
+     *            sorting request for theresults table
      */
     public VirtuosoReferencesSearchHelper(String subjectUri, PagingRequest pagingRequest, SortingRequest sortingRequest) {
 
@@ -51,8 +55,8 @@ public class VirtuosoReferencesSearchHelper extends AbstractSearchHelper {
     @Override
     protected String getOrderedQuery(List<Object> inParams) {
         String sparql =
-            "select distinct ?s where {?s ?p ?o. filter(isURI(?o) && ?o=" + (isValidIRI ? "?subjectUri" : "IRI(?subjectUri)")
-            + ")";
+                "select distinct ?s where {?s ?p ?o. filter(isURI(?o) && ?o=" + (isValidIRI ? "?subjectUri" : "IRI(?subjectUri)")
+                        + ")";
 
         if (sortPredicate != null && (Predicates.RDFS_LABEL.equals(sortPredicate) || Predicates.RDF_TYPE.equals(sortPredicate))) {
             sparql += " . OPTIONAL {?s ?sortPredicate ?oorderby }";
@@ -66,9 +70,9 @@ public class VirtuosoReferencesSearchHelper extends AbstractSearchHelper {
         if (Predicates.RDFS_LABEL.equals(sortPredicate)) {
 
             sparql +=
-                "(bif:either( bif:isnull(?oorderby) , "
-                + "(bif:lcase(bif:subseq (bif:replace (?s, '/', '#'), bif:strrchr (bif:replace (?s, '/', '#'), '#')+1))) , "
-                + "bif:lcase(?oorderby)))";
+                    "(bif:either( bif:isnull(?oorderby) , "
+                            + "(bif:lcase(bif:subseq (bif:replace (?s, '/', '#'), bif:strrchr (bif:replace (?s, '/', '#'), '#')+1))) , "
+                            + "bif:lcase(?oorderby)))";
             // kind of hack - ReferringPredicatesColumn class name is neither a predicate or URI.
             // It is not used in sparql so we can love with it for a while
         } else if (ReferringPredicatesColumn.class.getSimpleName().equals(sortPredicate)) {
@@ -84,7 +88,7 @@ public class VirtuosoReferencesSearchHelper extends AbstractSearchHelper {
      * SPARQL for unordered references of the subject.
      */
     private static final String REFERENCES_UNORDERED_SPARQL = "select distinct ?s where "
-        + "{?s ?p ?o. filter(isURI(?o) && ?o=?subjectUri)}";
+            + "{?s ?p ?o. filter(isURI(?o) && ?o=?subjectUri)}";
 
     @Override
     public String getUnorderedQuery(List<Object> inParams) {
@@ -97,7 +101,7 @@ public class VirtuosoReferencesSearchHelper extends AbstractSearchHelper {
      */
 
     private static final String REFERENCES_COUNT_SPARQL = "select count(distinct ?s) "
-        + "where {?s ?p ?o. filter(isURI(?o) && ?o=?subjectUri)}";
+            + "where {?s ?p ?o. filter(isURI(?o) && ?o=?subjectUri)}";
 
     @Override
     public String getCountQuery(List<Object> inParams) {
@@ -106,10 +110,13 @@ public class VirtuosoReferencesSearchHelper extends AbstractSearchHelper {
     }
 
     /**
-     * returns sparql query for getting predicates for reference subjects.
+     * returns sparql query for getting predicates for reference subjects. If the object is literal, it will be maximum of 2000
+     * characters long.
      *
-     * @param subjectUris subject uris of the references
-     * @param sourceUri resource subject URI.
+     * @param subjectUris
+     *            subject uris of the references
+     * @param sourceUri
+     *            resource subject URI.
      * @return SPARQL query for getting subjects data.
      */
     public String getSubjectsDataQuery(Collection<String> subjectUris, String sourceUri) {
@@ -122,9 +129,10 @@ public class VirtuosoReferencesSearchHelper extends AbstractSearchHelper {
         String subjectUrisCSV = SPARQLQueryUtil.urisToCSV(subjectUris, "subjectUriValue", subjectDataBindings);
         String sourceUriBinding = (isValidIRI ? "?sourceUri" : "IRI(?sourceUri)");
         String sparql =
-            "select * where {graph ?g {?s ?p ?o. filter (?s IN (" + subjectUrisCSV + ")) " + ". filter(?p = <"
-            + Predicates.RDF_TYPE + "> || <" + Predicates.RDFS_LABEL + "> || (isURI(?o) && ?o=" + sourceUriBinding
-            + "))}} ORDER BY ?s";
+                "select ?g ?s ?p bif:either(isLiteral(?o), bif:substring(str(?o), 1, " + WebConstants.MAX_OBJECT_LENGTH + "), ?o) as ?o "
+                        + "where {graph ?g {?s ?p ?o. filter (?s IN (" + subjectUrisCSV + ")) " + ". filter(?p = <"
+                        + Predicates.RDF_TYPE + "> || <" + Predicates.RDFS_LABEL + "> || (isURI(?o) && ?o=" + sourceUriBinding
+                        + "))}} ORDER BY ?s";
 
         return sparql;
     }
