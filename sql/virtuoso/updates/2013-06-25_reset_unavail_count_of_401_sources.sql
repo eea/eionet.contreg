644bc4d30d4b693fc486419eb9995ea7f773e225
Søren Roug <soren.roug@eea.europa.eu>
2013-06-26 08:22:23 +0000

    Typo in script

--- sql/virtuoso/updates/2013-06-25_reset_unavail_count_of_401_sources.sql
+++ sql/virtuoso/updates/2013-06-25_reset_unavail_count_of_401_sources.sql
@@ -4,4 +4,4 @@
 -- HARVEST that ended up with HTTP_CODE 401 (i.e. unauthorized).
 --
 
-UPDATE harvest_source SET count_unavail=0 WHERE harvest_source_id in (select harvest_source.harvest_source_id from harvest_source, harvest where harvest_source.last_harvest_id=harvest.harvest_id and harvest.http_code=401);
+UPDATE CR.cr3user.harvest_source SET count_unavail=0 WHERE harvest_source_id in (select harvest_source.harvest_source_id from harvest_source, harvest where harvest_source.last_harvest_id=harvest.harvest_id and harvest.http_code=401);
