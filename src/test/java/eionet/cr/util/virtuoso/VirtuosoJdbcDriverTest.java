01a32f48e8fbdf1888dd11c73bfcf7502f9c9481
heinlja <jaanus.heinlaid@gmail.com>
2014-01-18 20:51:58 +0200

    One more test for simple insert-select roundtrip. Refs #16639.

--- src/test/java/eionet/cr/util/virtuoso/VirtuosoJdbcDriverTest.java
+++ src/test/java/eionet/cr/util/virtuoso/VirtuosoJdbcDriverTest.java
@@ -27,14 +27,15 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
-import junit.framework.TestCase;
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Test;
 
 import virtuoso.jdbc4.VirtuosoConnectionPoolDataSource;
 import virtuoso.jdbc4.VirtuosoPooledConnection;
 import eionet.cr.config.GeneralConfig;
+import eionet.cr.test.helpers.CRDatabaseTestCase;
 import eionet.cr.util.sesame.SesameUtil;
 import eionet.cr.util.sql.SQLUtil;
 
@@ -43,10 +44,20 @@ import eionet.cr.util.sql.SQLUtil;
  *
  * @author Enriko Käsper
  */
-public class VirtuosoJdbcDriverTest extends TestCase {
+public class VirtuosoJdbcDriverTest extends CRDatabaseTestCase {
 
     /** Dummy graph URI. */
     private static final String DUMMY_GRAPH_URI = "http://test.virtuoso.jdbc.validity.com";
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see eionet.cr.test.helpers.CRDatabaseTestCase#getRDFXMLSeedFiles()
+     */
+    @Override
+    protected List<String> getRDFXMLSeedFiles() {
+        return Arrays.asList("obligations.rdf");
+    }
 
     /**
      * Test if CR uses correct Virtuoso JDBC driver. It shouldn't get "Too many open statements" error.
@@ -92,12 +103,12 @@ public class VirtuosoJdbcDriverTest extends TestCase {
     }
 
     /**
-     * Test simple insert into RDF_QUAD and a selecy afterwards.
+     * Test simple insert into RDF_QUAD and a SPARQL select afterwards.
      *
      * @throws SQLException
      */
     @Test
-    public void testSimpleInsertAndSparqlSelect() throws SQLException {
+    public void testSimpleInsertAndSPARQLSelect() throws SQLException {
 
         Connection conn = null;
         Statement stmt = null;
@@ -122,6 +133,47 @@ public class VirtuosoJdbcDriverTest extends TestCase {
             assertTrue("Expected the previously inserted triple to exist in triplestore", rs.next());
             String graph = "http://test.uri/graph";
             assertEquals("Expected previously inserted triple in " + graph, graph, rs.getString(1));
+        } catch (SQLException e) {
+            fail("Wasn't expecting this exception: " + e);
+            throw e;
+        } finally {
+            SQLUtil.close(rs);
+            SQLUtil.close(stmt);
+            SQLUtil.close(conn);
+        }
+    }
+
+    /**
+     * Test simple insert into RDF_QUAD and an SQL select afterwards.
+     *
+     * @throws SQLException
+     */
+    @Test
+    public void testSimpleInsertAndSQLSelect() throws SQLException {
+
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = SesameUtil.getSQLConnection();
+            stmt = conn.createStatement();
+            int i =
+                    stmt.executeUpdate("insert into DB.DBA.RDF_QUAD (S,P,O,G) values (" + "iri_to_id('http://test.uri/subject')"
+                            + ", iri_to_id('http://test.uri/predicate')" + ", iri_to_id('http://test.uri/object')"
+                            + ", iri_to_id('http://test.uri/graph'))");
+            assertEquals("Expect one triple to have been inserted!", 1, i);
+            SQLUtil.close(stmt);
+            SQLUtil.close(conn);
+
+            conn = SesameUtil.getSQLConnection();
+            stmt = conn.createStatement();
+            rs =
+                    stmt.executeQuery("select top(1) id_to_iri(G) from DB.DBA.RDF_QUAD where " + "S=iri_to_id('http://test.uri/subject') "
+                            + "and P=iri_to_id('http://test.uri/predicate') and O=iri_to_id('http://test.uri/object')");
+            assertTrue("Expected the previously inserted row to exist in DB.DBA.RDF_QUAD", rs.next());
+            String graph = "http://test.uri/graph";
+            assertEquals("Expected previously inserted DB.DBA.RDF_QUAD row with G=" + graph, graph, rs.getString(1));
         } catch (SQLException e) {
             fail("Wasn't expecting this exception: " + e);
             throw e;
