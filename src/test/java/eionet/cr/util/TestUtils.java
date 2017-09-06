57d7beb01f46ac08d2e10f365e1ef12c09634d5d
George Sofianos <gso@eworx.gr>
2017-09-06 19:00:50 +0300

    Small update. Refs #80203

---
+++ src/test/java/eionet/cr/util/TestUtils.java
@@ -0,0 +1,31 @@
+package eionet.cr.util;
+
+import org.dbunit.database.DatabaseConfig;
+import org.dbunit.database.DatabaseConnection;
+import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.dbunit.ext.mysql.MySqlDataTypeFactory;
+import org.dbunit.operation.DatabaseOperation;
+
+import javax.sql.DataSource;
+import java.io.File;
+import java.sql.Connection;
+
+/**
+ *
+ *
+ */
+public class TestUtils {
+
+    public static void setUpDatabase(DataSource db, String dataset) throws Exception {
+        Connection dsConnection = db.getConnection();
+        IDatabaseConnection connection = new DatabaseConnection(dsConnection);
+        DatabaseConfig config = connection.getConfig();
+        config.setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new MySqlDataTypeFactory());
+        config.setProperty(DatabaseConfig.FEATURE_ALLOW_EMPTY_FIELDS, Boolean.TRUE);
+        File file = new File(db.getClass().getClassLoader().getResource(dataset).getFile());
+        IDataSet dataSet = new FlatXmlDataSetBuilder().build(file);
+        DatabaseOperation.CLEAN_INSERT.execute(connection, dataSet);
+    }
+}
