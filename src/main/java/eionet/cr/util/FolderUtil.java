a6ef9a6eababe14e7e66057e95da4ce6f13bef71
Jaanus Heinlaid <jaanus.heinlaid@gmail.com>
2019-06-14 13:32:16 +0300

    Bugfix: avoid NPE when harvesting root folder.

--- src/main/java/eionet/cr/util/FolderUtil.java
+++ src/main/java/eionet/cr/util/FolderUtil.java
@@ -20,16 +20,15 @@
  */
 package eionet.cr.util;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.apache.commons.lang.StringUtils;
-
 import eionet.cr.config.GeneralConfig;
 import eionet.cr.dao.DAOException;
 import eionet.cr.dao.DAOFactory;
 import eionet.cr.dao.FolderDAO;
 import eionet.cr.web.security.CRUser;
+import org.apache.commons.lang.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -172,7 +171,7 @@ public final class FolderUtil {
      */
     public static String extractPathInSpecialFolder(String uri, String mainFolder) {
 
-        String result = null;
+        String result = "";
         String appHome = GeneralConfig.getRequiredProperty(GeneralConfig.APPLICATION_HOME_URL);
         if (uri.startsWith(appHome + "/" + mainFolder + "/")) {
             result = StringUtils.substringAfter(uri, appHome + "/" + mainFolder + "/");
