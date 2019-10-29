4d27fe71103526c54d3400424b07ce932e6333eb
Claudia Ifrim <ifrim.claudia@gmail.com>
2019-10-29 18:33:33 +0200

    wait other containers to be removed

--- prepare-tmp.sh
+++ prepare-tmp.sh
@@ -12,6 +12,9 @@ mkdir /var/jenkins_home/worker/tmp_cr
 
 sed -i "s+^config.docker.sharedVolume=.*+config.docker.sharedVolume=$WORKERDIR/tmp_cr+g" tests.properties
 
-BUILDTIME=$(date '+%Y-%m-%dT%H%M')
+until [ "docker ps -a | grep virtuoso | cut -d " " -f1 2> /dev/null)" == "" ]; do
+    echo "Waiting for ports to be available ..."
+    sleep 10;
+done;
 
 exit 0
