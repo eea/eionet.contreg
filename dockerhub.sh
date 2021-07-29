91368c77882a02dcb8cd790bfc48f369e1631ecd
Maria Favva <maria.favva@gmail.com>
2021-07-29 16:03:58 +0300

    removed build command from dockerhub.sh. Refs#107525

--- dockerhub.sh
+++ dockerhub.sh
@@ -4,4 +4,4 @@ APP=eworxeea/contreg
 BUILDTIME=$(date '+%Y-%m-%dT%H%M')
 
 # docker build runs only if tests are successful
-mvn clean install && docker build -t $APP:latest . &&  docker tag $APP:latest $APP:$BUILDTIME && docker push $APP:latest && docker push $APP:$BUILDTIME
+docker build -t $APP:latest . &&  docker tag $APP:latest $APP:$BUILDTIME && docker push $APP:latest && docker push $APP:$BUILDTIME
